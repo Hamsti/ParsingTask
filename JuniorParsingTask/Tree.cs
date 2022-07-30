84e0d3e5289b59571dbcb0a3578b48d9b56192f0
Oleg_Belous <oleg_belous@epam.com>
2022-07-30 11:25:37 +0300

    moved TryGetNode to Tree class

--- JuniorParsingTask/Tree.cs
+++ JuniorParsingTask/Tree.cs
@@ -2,11 +2,38 @@
 
 public class Tree
 {
-    public readonly Node Root;
+    public readonly Node? Root;
     public int Size { get; set; }
 
     public Tree(Node root)
     {
         Root = root;
+    }
+
+    public bool TryGetNode(string value, out Node? node)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        node = GetEnumerableByRecursion(Root).FirstOrDefault(node => node is not null && node.Value.Contains(value));
+
+        return node is not null;
+    }
+
+    private static IEnumerable<Node?> GetEnumerableByRecursion(Node? currentNode)
+    {
+        yield return currentNode;
+
+        if (currentNode is null)
+        {
+            yield break;
+        }
+        
+        foreach (var child in currentNode.Children.SelectMany(GetEnumerableByRecursion))
+        {
+            yield return child;
+        }
     }
 }
