fc2630445220287fcfdfa6afff29005ca762c19d
Oleg_Belous <oleg_belous@epam.com>
2022-07-30 11:18:58 +0300

    Implemented TryGetNode(..) method with IEnumarable

--- JuniorParsingTask/Node.cs
+++ JuniorParsingTask/Node.cs
@@ -1,6 +1,8 @@
-﻿namespace JuniorParsingTask;
+﻿using System.Collections;
 
-public class Node
+namespace JuniorParsingTask;
+
+public class Node : IEnumerable<Node>
 {
     public readonly string Value;
     public readonly List<Node> Children;
@@ -18,6 +20,27 @@ public class Node
 
     public void AddChildren(params Node[] children)
     {
-        Children.AddRange(children); 
+        Children.AddRange(children);
+    }
+
+    public bool TryGetNode(string value, out Node? node)
+    {
+        node = this.FirstOrDefault(node => node.Value.Contains(value));
+
+        return node is not null;
+    }
+
+    public IEnumerator<Node> GetEnumerator() => GetEnumerableByRecursion(this).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private static IEnumerable<Node> GetEnumerableByRecursion(Node currentNode)
+    {
+        yield return currentNode;
+
+        foreach (var child in currentNode.Children.SelectMany(GetEnumerableByRecursion))
+        {
+            yield return child;
+        }
     }
 }
